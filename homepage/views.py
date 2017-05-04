c092c0f08a84767c766bc0160c8e045260dfdff6
Melanie <pinqingkan@Pinqings-MacBook-Pro-13.local>
2017-05-04 19:38:13 -0400

    updated homepage template

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+#from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-	return HttpResponse("Hey!")
+def highlights(request):
+	return render(request, 'homepage.html')
