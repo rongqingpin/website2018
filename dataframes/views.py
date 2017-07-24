d0a655d3b5db1b50f6edb87167b3ea7c38ac477b
MelanieZ <kanpq@hotmail.com>
2017-07-24 09:40:42 -0400

    outlined iosapp project

---
+++ dataframes/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+
+# Create your views here.
+def iosapps(request):
+	return render(request, 'iosapps.html')
--- website/settings.py
+++ website/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'homepage',
     'timeseries',
     'images',
+    'dataframes',
     'about',
 ]
 
